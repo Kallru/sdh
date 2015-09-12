ef2bc2b21726c2d3cbd91bb3ff3a87d4d90e3e88
loader7406 <rudals7406@hanmail.net>
2015-09-12 12:02:16 +0900

    150912 BMP

---
+++ PKM/150829/150829/150829.cpp
@@ -0,0 +1,396 @@
+// 150829.cpp : 응용 프로그램에 대한 진입점을 정의합니다.
+//
+
+#include "stdafx.h"
+#include "150829.h"
+
+using namespace std;
+#include <stdio.h>
+
+#define MAX_LOADSTRING 100
+
+// 전역 변수:
+HINSTANCE hInst;								// 현재 인스턴스입니다.
+TCHAR szTitle[MAX_LOADSTRING];					// 제목 표시줄 텍스트입니다.
+TCHAR szWindowClass[MAX_LOADSTRING];			// 기본 창 클래스 이름입니다.
+
+BYTE*	g_pImageBuffer;
+
+int	g_Width, g_Height;
+
+int	g_nFileType;
+
+struct COLOR_
+{
+	UINT r;
+	UINT g;
+	UINT b;
+
+};
+// 이 코드 모듈에 들어 있는 함수의 정방향 선언입니다.
+ATOM				MyRegisterClass(HINSTANCE hInstance);
+BOOL				InitInstance(HINSTANCE, int);
+LRESULT CALLBACK	WndProc(HWND, UINT, WPARAM, LPARAM);
+INT_PTR CALLBACK	About(HWND, UINT, WPARAM, LPARAM);
+
+bool				ReadImage(LPCWSTR lpFileName);
+
+int APIENTRY _tWinMain(_In_ HINSTANCE hInstance,
+                     _In_opt_ HINSTANCE hPrevInstance,
+                     _In_ LPTSTR    lpCmdLine,
+                     _In_ int       nCmdShow)
+{
+	UNREFERENCED_PARAMETER(hPrevInstance);
+	UNREFERENCED_PARAMETER(lpCmdLine);
+
+ 	// TODO: 여기에 코드를 입력합니다.
+	MSG msg;
+	HACCEL hAccelTable;
+
+	// 전역 문자열을 초기화합니다.
+	LoadString(hInstance, IDS_APP_TITLE, szTitle, MAX_LOADSTRING);
+	LoadString(hInstance, IDC_MY150829, szWindowClass, MAX_LOADSTRING);
+	MyRegisterClass(hInstance);
+
+	// 응용 프로그램 초기화를 수행합니다.
+	if (!InitInstance (hInstance, nCmdShow))
+	{
+		return FALSE;
+	}
+
+	hAccelTable = LoadAccelerators(hInstance, MAKEINTRESOURCE(IDC_MY150829));
+
+	// 기본 메시지 루프입니다.
+	while (GetMessage(&msg, NULL, 0, 0))
+	{
+		if (!TranslateAccelerator(msg.hwnd, hAccelTable, &msg))
+		{
+			TranslateMessage(&msg);
+			DispatchMessage(&msg);
+		}
+	}
+
+	return (int) msg.wParam;
+}
+
+
+
+//
+//  함수: MyRegisterClass()
+//
+//  목적: 창 클래스를 등록합니다.
+//
+ATOM MyRegisterClass(HINSTANCE hInstance)
+{
+	WNDCLASSEX wcex;
+
+	wcex.cbSize = sizeof(WNDCLASSEX);
+
+	wcex.style			= CS_HREDRAW | CS_VREDRAW;
+	wcex.lpfnWndProc	= WndProc;
+	wcex.cbClsExtra		= 0;
+	wcex.cbWndExtra		= 0;
+	wcex.hInstance		= hInstance;
+	wcex.hIcon			= LoadIcon(hInstance, MAKEINTRESOURCE(IDI_MY150829));
+	wcex.hCursor		= LoadCursor(NULL, IDC_ARROW);
+	wcex.hbrBackground	= (HBRUSH)(COLOR_WINDOW+1);
+	wcex.lpszMenuName	= MAKEINTRESOURCE(IDC_MY150829);
+	wcex.lpszClassName	= szWindowClass;
+	wcex.hIconSm		= LoadIcon(wcex.hInstance, MAKEINTRESOURCE(IDI_SMALL));
+
+	return RegisterClassEx(&wcex);
+}
+
+//
+//   함수: InitInstance(HINSTANCE, int)
+//
+//   목적: 인스턴스 핸들을 저장하고 주 창을 만듭니다.
+//
+//   설명:
+//
+//        이 함수를 통해 인스턴스 핸들을 전역 변수에 저장하고
+//        주 프로그램 창을 만든 다음 표시합니다.
+//
+BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
+{
+   HWND hWnd;
+
+   hInst = hInstance; // 인스턴스 핸들을 전역 변수에 저장합니다.
+
+   hWnd = CreateWindow(szWindowClass, szTitle, WS_OVERLAPPEDWINDOW,
+      CW_USEDEFAULT, 0, CW_USEDEFAULT, 0, NULL, NULL, hInstance, NULL);
+
+   if (!hWnd)
+   {
+      return FALSE;
+   }
+
+   if (!ReadImage(L"data/test.bmp"))
+   {
+	   MessageBoxA(hWnd, "CAN'T LOAD", "FILE LOAD FAIL", MB_OK);
+   }
+
+   ShowWindow(hWnd, nCmdShow);
+   UpdateWindow(hWnd);
+
+   // ReadFile
+   
+   return TRUE;
+}
+
+//
+//  함수: WndProc(HWND, UINT, WPARAM, LPARAM)
+//
+//  목적:  주 창의 메시지를 처리합니다.
+//
+//  WM_COMMAND	- 응용 프로그램 메뉴를 처리합니다.
+//  WM_PAINT	- 주 창을 그립니다.
+//  WM_DESTROY	- 종료 메시지를 게시하고 반환합니다.
+//
+//
+LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+	int wmId, wmEvent;
+	PAINTSTRUCT ps;
+	HDC hdc;
+
+	switch (message)
+	{
+	case WM_COMMAND:
+		wmId = LOWORD(wParam);
+		wmEvent = HIWORD(wParam);
+		// 메뉴 선택을 구문 분석합니다.
+		switch (wmId)
+		{
+		case IDM_ABOUT:
+			DialogBox(hInst, MAKEINTRESOURCE(IDD_ABOUTBOX), hWnd, About);
+			break;
+		case IDM_EXIT:
+			DestroyWindow(hWnd);
+			break;
+		default:
+			return DefWindowProc(hWnd, message, wParam, lParam);
+		}
+		break;
+	case WM_PAINT:
+	{
+		hdc = BeginPaint(hWnd, &ps);
+		// TODO: 여기에 그리기 코드를 추가합니다.
+
+		//auto test = RGB(255, 255, 255);
+		//SetPixel(hdc,위치X,위치Y,색깔값)
+
+		int nStartX = 100;
+		int nStartY = 100;
+
+		int nIdx;
+
+		for (int y = 0; y < g_Height; ++y)
+		{
+			for (int x = 0; x < g_Width; ++x)
+			{
+				//nIdx = ((y * g_Width * 3) + x * 3);
+				nIdx = ((y * g_Width * g_nFileType) + x * g_nFileType);
+
+				auto pixel = 
+					RGB(
+					g_pImageBuffer[nIdx + 0],
+					g_pImageBuffer[nIdx + 1],
+					g_pImageBuffer[nIdx + 2]);
+									
+				SetPixel(hdc, nStartX+x, nStartY+y, pixel);
+			}
+			int n = 0;
+		}
+		EndPaint(hWnd, &ps);
+		break;
+	}
+	case WM_DESTROY:
+		delete[]	g_pImageBuffer;
+		PostQuitMessage(0);
+		break;
+	default:
+		return DefWindowProc(hWnd, message, wParam, lParam);
+	}
+	return 0;
+}
+
+// 정보 대화 상자의 메시지 처리기입니다.
+INT_PTR CALLBACK About(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
+{
+	UNREFERENCED_PARAMETER(lParam);
+	switch (message)
+	{
+	case WM_INITDIALOG:
+		return (INT_PTR)TRUE;
+
+	case WM_COMMAND:
+		if (LOWORD(wParam) == IDOK || LOWORD(wParam) == IDCANCEL)
+		{
+			EndDialog(hDlg, LOWORD(wParam));
+			return (INT_PTR)TRUE;
+		}
+		break;
+	}
+	return (INT_PTR)FALSE;
+}
+
+bool	ReadImage(LPCWSTR lpFileName)
+{
+	g_nFileType = -1;
+
+	// 확장자 분리
+
+	char cFile[256];
+
+	int nLen = (int)wcslen(lpFileName);
+	wcstombs(cFile, lpFileName, nLen + 1);
+
+	char	ext[16];
+	_splitpath(cFile, NULL, NULL, NULL, ext);
+	
+	if (strcmp(ext, ".hiy") == 0)
+	{
+		g_nFileType = 3;
+	}
+	else if (strcmp(ext, ".bmp") == 0)
+	{
+		g_nFileType = 1;
+	}
+	else
+	{
+		return false;
+	}
+
+	HANDLE hRead = CreateFile(lpFileName, GENERIC_READ, 0, NULL, OPEN_EXISTING, 0, NULL);
+
+	DWORD	nResultSize = 0;
+	BYTE*	Temp; 
+
+	switch (g_nFileType)
+	{
+	case 3:
+	{
+			  BYTE	header[11] = { 0, };
+
+			  if (ReadFile(hRead, &header, 11, &nResultSize, NULL))
+			  {
+				  char tag[3];
+
+				  CopyMemory((void*)&tag, &header[0], 3);
+
+				  CopyMemory((void*)&g_Width, &header[3], 4);
+				  CopyMemory((void*)&g_Height, &header[7], 4);
+
+				  Temp = new	BYTE[sizeof(BYTE)* g_Width * g_Height * 12];
+				  ZeroMemory(Temp, sizeof(BYTE)* g_Width * g_Height * 12);
+
+				  if (ReadFile(hRead, Temp, g_Width * g_Height * 12, &nResultSize, NULL) == true)
+				  {
+					  int a = 0;
+				  }
+			  }
+
+			  g_pImageBuffer = new	BYTE[sizeof(BYTE)* g_Width * g_Height * 3];
+			  ZeroMemory(g_pImageBuffer, sizeof(BYTE)* g_Width * g_Height * 3);
+
+			  nResultSize = nResultSize / 4;
+
+			  for (int i = 0; i < nResultSize; ++i)
+			  {
+				  g_pImageBuffer[i] = Temp[i * 4];
+			  }
+
+			  delete[] Temp;
+
+			  break;
+	}
+
+	case 1:
+	{
+			  BYTE	header[40] = { 0, };
+
+			  int nPixelData = 0;
+			  int nHeaderSize = 0;
+
+			  // READ HEADER INFO
+			  if (ReadFile(hRead, &header, 14, &nResultSize, NULL))
+			  {
+				 /* CopyMemory((void*)&nPixelData, &header[10], 4);
+
+				  CopyMemory((void*)&nHeaderSize, &header[14], 4);
+
+				  CopyMemory((void*)&g_Width, &header[18], 4);
+
+				  CopyMemory((void*)&g_Height, &header[22], 4);
+
+				  if (ReadFile(hRead, &header, nPixelData - 25, &nResultSize, NULL))
+				  {
+					  int a = 0;
+				  }
+				  Temp = new	BYTE[sizeof(BYTE)* g_Width * g_Height * 3];
+				  ZeroMemory(Temp, sizeof(BYTE)* g_Width * g_Height * 3);
+
+				  if (ReadFile(hRead, Temp, g_Width * g_Height * 3, &nResultSize, NULL) == true)
+				  {
+					  int a = 0;
+				  }*/
+
+				  // READ HEADER
+				  if (ReadFile(hRead, &header, 40, &nResultSize, NULL))
+				  {
+					  CopyMemory((void*)&nHeaderSize, &header[0], 4);
+
+					  CopyMemory((void*)&g_Width, &header[4], 4);
+					  CopyMemory((void*)&g_Height, &header[8], 4);
+				  }
+
+				  // COLOR OUT
+				  if (ReadFile(hRead, &header, 4, &nResultSize, NULL))
+				  {
+
+				  }
+
+				  Temp = new	BYTE[sizeof(BYTE)* g_Width * g_Height * 4];
+				  ZeroMemory(Temp, sizeof(BYTE)* g_Width * g_Height * 4);
+
+				  // READ PIXEL
+				  if (ReadFile(hRead, Temp, g_Width * g_Height * 4, &nResultSize, NULL))
+				  {
+					  int a = 0;
+				  }
+			  }
+
+			  g_pImageBuffer = new	BYTE[sizeof(BYTE)* g_Width * g_Height * 4];
+			  ZeroMemory(g_pImageBuffer, sizeof(BYTE)* g_Width * g_Height * 4);
+
+			  for (int i = 0; i < g_Width * g_Height; ++i)
+			  {
+				  /*RGBQUAD	color_info;
+				  
+				  CopyMemory((void*)&color_info, &Temp[i*4], 4);
+
+				  COLOR_	color;
+				  color.r = color_info.rgbRed;
+				  color.g = color_info.rgbGreen;
+				  color.b = color_info.rgbBlue;
+
+				  memcpy(
+					  &g_pImageBuffer,
+					  &color,
+					  sizeof(color));*/
+
+				  g_pImageBuffer[i] = Temp[i * 4];
+			  }
+
+			  delete[] Temp;
+
+			  break;
+	}
+
+	}
+
+	CloseHandle(hRead);
+
+	return	true;
+}
