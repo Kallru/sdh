2f48dbd8477d6065e9212da88bd4977b36a89852
Hwi Jae, Shin <pkallru@gmail.com>
2015-11-21 15:53:13 +0900

    기본 통신
    ㄴㅇㄹ

--- CodeTest/NetworkAsio/AsioServer/AsioServer.cpp
+++ CodeTest/NetworkAsio/AsioServer/AsioServer.cpp
@@ -5,39 +5,45 @@
 
 #include <cstdlib>
 #include <iostream>
-#include <thread>
-#include <utility>
+#include <boost/bind.hpp>
+#include <boost/smart_ptr.hpp>
 #include <boost/asio.hpp>
+#include <boost/thread/thread.hpp>
+
+#include <vector>
 
 using boost::asio::ip::tcp;
 
 const int max_length = 1024;
 
-void session(tcp::socket sock)
+typedef boost::shared_ptr<tcp::socket> socket_ptr;
+
+std::vector<socket_ptr> g_clinets;
+
+void session(socket_ptr sock)
 {
-	try
+	for (;;)
 	{
-		for (;;)
+		char data[max_length];
+		::memset(data, 0, max_length);
+
+		boost::system::error_code error;
+		size_t length = sock->read_some(boost::asio::buffer(data), error);
+		if (error == boost::asio::error::eof)
+			break; // Connection closed cleanly by peer.
+		else if (error)
+			throw boost::system::system_error(error); // Some other error.
+
+		// boost::asio::write(*sock, boost::asio::buffer(data, length));
+
+		for (int i = 0; i < g_clinets.size(); ++i)
 		{
-			char data[max_length];
-
-			boost::system::error_code error;
-			size_t length = sock.read_some(boost::asio::buffer(data), error);
-			if (error == boost::asio::error::eof)
-				break; // Connection closed cleanly by peer.
-			else if (error)
-				throw boost::system::system_error(error); // Some other error.
-
-			boost::asio::write(sock, boost::asio::buffer(data, length));
-
-			std::cout << "Client Data is :";
-			std::cout.write(data, length);
-			std::cout << "\n";
+			boost::asio::write(*g_clinets[i], boost::asio::buffer(data, length));
 		}
-	}
-	catch (std::exception& e)
-	{
-		std::cerr << "Exception in thread: " << e.what() << "\n";
+
+		std::cout << "Client Data is :";
+		std::cout.write(data, length);
+		std::cout << "\n";
 	}
 }
 
@@ -46,30 +52,20 @@ void server(boost::asio::io_service& io_service, unsigned short port)
 	tcp::acceptor a(io_service, tcp::endpoint(tcp::v4(), port));
 	for (;;)
 	{
-		tcp::socket sock(io_service);
-		a.accept(sock);
-		std::thread(session, std::move(sock)).detach();
+		socket_ptr sock(new tcp::socket(io_service));
+		a.accept(*sock);
+
+		g_clinets.push_back(sock);
+		boost::thread t(boost::bind(session, sock));
 	}
 }
 
 int main(int argc, char* argv[])
 {
-	try
-	{
-		if (argc != 2)
-		{
-			std::cerr << "Usage: blocking_tcp_echo_server <port>\n";
-			return 1;
-		}
+	int port = 123456;
 
-		boost::asio::io_service io_service;
-
-		server(io_service, std::atoi(argv[1]));
-	}
-	catch (std::exception& e)
-	{
-		std::cerr << "Exception: " << e.what() << "\n";
-	}
+	boost::asio::io_service io_service;
+	server(io_service, port);
 
 	return 0;
 }
--- CodeTest/NetworkAsio/NetworkAsio/NetworkAsio.cpp
+++ CodeTest/NetworkAsio/NetworkAsio/NetworkAsio.cpp
@@ -6,37 +6,59 @@
 #include <cstring>
 #include <iostream>
 #include <boost/asio.hpp>
+#include <boost/thread/thread.hpp>
+#include <boost/bind.hpp>
+#include <boost/smart_ptr.hpp>
 
 using boost::asio::ip::tcp;
 
 enum { MAX_LENGTH = 1024 };
 
+typedef boost::shared_ptr<tcp::socket> socket_ptr;
+
+void recv(socket_ptr sock)
+{
+	while (true)
+	{
+		char reply[MAX_LENGTH];
+		::memset(reply, 0, MAX_LENGTH);
+
+		boost::system::error_code error;
+		size_t reply_length = sock->read_some(boost::asio::buffer(reply), error);
+
+		std::cout << "\n";
+		std::cout << "Reply is: ";
+		std::cout.write(reply, reply_length);
+		std::cout << "\n";
+	}
+}
+
 int main(int argc, char* argv[])
 {
 	if (argc != 3)
 	{
-		std::cerr << "Usage: blocking_tcp_echo_client <host> <port>\n";
-		return 1;
+		//std::cerr << "Usage: blocking_tcp_echo_client <host> <port>\n";
+		//return 1;
 	}
 
 	boost::asio::io_service io_service;
 
-	tcp::socket socket(io_service);
+	socket_ptr pSocket(new tcp::socket(io_service));
 	tcp::resolver resolver(io_service);
-	boost::asio::connect(socket, resolver.resolve({ argv[1], argv[2] }));
+	//boost::asio::connect(socket, resolver.resolve({ argv[1], argv[2] }));
+	boost::asio::connect(*pSocket, resolver.resolve({ "192.168.1.57", "123456" }));
 
-	std::cout << "Enter message: ";
-	char request[MAX_LENGTH];
-	std::cin.getline(request, MAX_LENGTH);
-	size_t request_length = std::strlen(request);
-	boost::asio::write(socket, boost::asio::buffer(request, request_length) );
+	// 리시브 스레드 생성
+	boost::thread rc(boost::bind(recv, pSocket));
 
-	char reply[MAX_LENGTH];
-	::memset(reply, 0, MAX_LENGTH);
-	size_t reply_length = boost::asio::read(socket, boost::asio::buffer(reply, request_length));
-	std::cout << "Reply is: ";
-	std::cout.write(reply, reply_length);
-	std::cout << "\n";
+	while (true)
+	{
+		std::cout << "Enter message: ";
+		char request[MAX_LENGTH];
+		std::cin.getline(request, MAX_LENGTH);
+		size_t request_length = std::strlen(request);
+		boost::asio::write(*pSocket, boost::asio::buffer(request, request_length));
+	}	
 
 	return 0;
 }
